35311b4000dee0f54051d0402eee583eb11e84c6
ThiagoSouza17 <thiagossouza07@gmail.com>
2022-01-26 21:27:17 -0200

    Primeira Classe e Controller

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,23 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Controllers
+{
+
+    [ApiController]
+    [Route("[Controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+        }
+    }
+}
