1ec5043057b357f9fb4e8f2b602b034dd4924771
ThiagoSouza17 <thiagossouza07@gmail.com>
2022-01-26 22:02:46 -0200

    Implementa método RecuperaFilmePorId

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -27,5 +27,11 @@ namespace FilmesAPI.Controllers
         {
             return filmes;
         }
+
+        [HttpGet("{id}")]
+        public Filme RecuperaFilmesPorId(int id)
+        {
+            return filmes.FirstOrDefault(filme => filme.Id == id);
+        }
     }
 }
