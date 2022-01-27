d8f51c192c10b60544e4dd678730cd017a4c8f41
ThiagoSouza17 <thiagossouza07@gmail.com>
2022-01-27 15:54:07 -0200

    Implementa a lógica de banco de dados no Controller

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -1,4 +1,5 @@
-﻿using FilmesAPI.Models;
+﻿using FilmesApi.Data;
+using FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -12,27 +13,31 @@ namespace FilmesApi.Controllers
     [Route("[controller]")]
     public class FilmeController : ControllerBase
     {
-        private static List<Filme> filmes = new List<Filme>();
-        private static int id = 1;
+        private FilmeContext _context;
+
+        public FilmeController(FilmeContext context)
+        {
+            _context = context;
+        }
 
         [HttpPost]
         public IActionResult AdicionaFilme([FromBody] Filme filme)
         {
-            filme.Id = id++;
-            filmes.Add(filme);
+            _context.Filmes.Add(filme);
+            _context.SaveChanges();
             return CreatedAtAction(nameof(RecuperaFilmesPorId), new { Id = filme.Id }, filme);
         }
 
         [HttpGet]
         public IActionResult RecuperaFilmes()
         {
-            return Ok(filmes);
+            return Ok(_context.Filmes);
         }
 
         [HttpGet("{id}")]
         public IActionResult RecuperaFilmesPorId(int id)
         {
-            Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
             if(filme != null)
             {
                 return Ok(filme);
